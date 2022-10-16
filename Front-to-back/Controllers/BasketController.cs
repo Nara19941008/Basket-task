e2106d5efb2bc97eb06a7e1efa82970dffcb89bd
Nara19941008 <narminim@code.edu.az>
2022-10-16 22:42:06 +0300

    Added bio table

---
+++ Front-to-back/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Front_to_back.Data;
+using Front_to_back.Models;
+using Front_to_back.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Front_to_back.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<BasketVM> basketItems = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            List<BasketDetailVM> basketDetail = new List<BasketDetailVM>();
+
+            foreach (var item in basketItems)
+            {
+                Product product = await _context.Products.Where(m => m.Id == item.Id && !m.IsDeleted).FirstOrDefaultAsync();
+
+                BasketDetailVM newBasket = new BasketDetailVM
+                {
+                    Title = product.Title,
+                    Image = product.Image,
+                    Price = product.Price,
+                    Count = item.Count,
+                    Total = product.Price * item.Count
+                };
+
+                basketDetail.Add(newBasket);
+            }
+
+            return View(basketDetail);
+        }
+    }
+}
---
+++ Front-to-back/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Front_to_back.Models
+{
+    public class Accessory : BaseEntity
+    {
+        public class Product : BaseEntity
+        {
+            public string Image { get; set; }
+           
+            public string Title { get; set; }
+
+            [Column(TypeName = "decimal(14,4)")]
+           
+            public decimal Price { get; set; }
+        }
+    }
+}
